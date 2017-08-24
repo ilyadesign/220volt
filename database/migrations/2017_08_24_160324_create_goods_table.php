bc29ed41cf4fcf3640239dd7dbf9d972e3ad7e6e
Ilya Emelyanov <mail@ilyadesign.ru>
2017-08-24 18:22:00 +0300

    up

--- database/migrations/2017_08_24_160324_create_goods_table.php
+++ database/migrations/2017_08_24_160324_create_goods_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateGoodsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+
+        //
+        Schema::create('goods', function (Blueprint $table)
+        {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->integer('vendor_id')->nullable(false);
+            $table->decimal('price',7,2);
+            $table->text('description');
+
         });
     }
 
@@ -30,6 +33,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
+        Schema::dropIfExists('goods');
     }
 }
